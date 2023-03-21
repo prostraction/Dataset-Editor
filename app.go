84ef53d8c06d3742d4d78f00ff8b4634065aa242
prostraction <47314760+prostraction@users.noreply.github.com>
2023-03-21 18:24:26 +0300

    Init wails project

---
+++ app.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+
+	"github.com/wailsapp/wails/v2/pkg/runtime"
+)
+
+// App struct
+type App struct {
+	ctx context.Context
+}
+
+// NewApp creates a new App application struct
+func NewApp() *App {
+	return &App{}
+}
+
+// startup is called when the app starts. The context is saved
+// so we can call the runtime methods
+func (a *App) startup(ctx context.Context) {
+	a.ctx = ctx
+}
+
+func (a *App) SetDirectoryDialog() string {
+	result, _ := runtime.OpenDirectoryDialog(a.ctx, runtime.OpenDialogOptions{
+		DefaultDirectory:           ".",
+		Title:                      "123",
+		ShowHiddenFiles:            true,
+		CanCreateDirectories:       false,
+		ResolvesAliases:            false,
+		TreatPackagesAsDirectories: true})
+	if len(result) > 0 {
+		return result
+	} else {
+		return "Dialog cancelled"
+	}
+
+}
