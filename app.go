1376366651991e4dd33eb6135344a201ebcefb12
prostraction <47314760+prostraction@users.noreply.github.com>
2023-09-01 23:38:28 +0300

    Task Schedule, correct async calls

--- app.go
+++ app.go
@@ -9,7 +9,10 @@ import (
 
 // App struct
 type App struct {
-	ctx context.Context
+	ctx            context.Context
+	lastDir        string
+	isTaskFinished bool
+	taskProcent    float32
 }
 
 // NewApp creates a new App application struct
@@ -24,33 +27,47 @@ func (a *App) startup(ctx context.Context) {
 }
 
 func (a *App) SetDirectoryDialog() string {
+	if a.lastDir == "" {
+		a.lastDir = "."
+	}
 	result, _ := runtime.OpenDirectoryDialog(a.ctx, runtime.OpenDialogOptions{
-		DefaultDirectory:           ".",
+		DefaultDirectory:           a.lastDir,
 		Title:                      "Choose directory",
 		ShowHiddenFiles:            true,
 		CanCreateDirectories:       true,
 		ResolvesAliases:            false,
 		TreatPackagesAsDirectories: true})
 	if len(result) > 0 {
+		a.lastDir = result
 		return result
 	}
 	return "Dialog cancelled"
 }
 
+func (a *App) IsTaskFinished() bool {
+	return a.isTaskFinished
+}
+
 func (a *App) StartMergeProcess(dir_in_1 string, dir_in_2 string, dir_merged string) bool {
+	a.isTaskFinished = false
 	fmt.Println("Merge called")
 	ProcessMerge(dir_in_1+"\\", dir_in_2+"\\", dir_merged+"\\")
+	a.isTaskFinished = true
 	return true
 }
 
 func (a *App) StartCropProcess(dir_in_1 string, dir_result string, x int, y int) bool {
+	a.isTaskFinished = false
 	fmt.Println("Crop called")
 	ProcessCut(dir_in_1+"\\", dir_result+"\\", x, y)
+	a.isTaskFinished = true
 	return true
 }
 
 func (a *App) StartProcessBrightness(dir_in_1 string, dir_result string, factor int) bool {
+	a.isTaskFinished = false
 	fmt.Println("Brightness called")
 	ProcessBrightness(dir_in_1+"\\", dir_result+"\\", factor)
+	a.isTaskFinished = true
 	return true
 }
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import './App.css';
+import { IsTaskFinished } from '../wailsjs/go/main/App';
 import { SetDirectoryDialog } from '../wailsjs/go/main/App';
 import { StartMergeProcess } from '../wailsjs/go/main/App';
 import { StartCropProcess } from '../wailsjs/go/main/App';
@@ -9,11 +10,24 @@ export default class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      isRunning: 0,
+      completedTasks: 0,
+      allTasks: 0,
+      viewTasks: false,
+      currentOperationLabel: 'Ready.',
       operation: 0,
+      operations: [],
+      humanReadableOperations: [],
       checked: [1, 0, 0],
       dir1: 'Files directory', dir1Selected: false,
+      dirs1: [],
       dir2: 'Second files directory', dir2Selected: false,
+      dirs2: [],
       dir3: 'Result Directory', dir3Selected: false,
+      dirs3: [],
+      x: [],
+      y: [],
+      br: [],
     };
 
     this.SetDirectory1 = this.SetDirectory1.bind(this);
@@ -21,6 +35,10 @@ export default class App extends React.Component {
     this.SetDirectory3 = this.SetDirectory3.bind(this);
     this.ChangeMode = this.ChangeMode.bind(this);
     this.Progress = this.Progress.bind(this);
+    this.ProgressOneItem = this.ProgressOneItem.bind(this);
+    this.AddToQueue = this.AddToQueue.bind(this);
+    this.ViewTasks = this.ViewTasks.bind(this);
+    this.DeleteTask = this.DeleteTask.bind(this);
   }
 
   SetDirectory1 = () => {
@@ -73,8 +91,7 @@ export default class App extends React.Component {
     this.setState({ checked: checked, operation: flag, dir1: dirText });
   };
 
-  Progress = () => {
-    console.log(this.state.operation)
+  AddToQueue = () => {
     let ready = true;
     if (!this.state.dir1Selected) {document.body.querySelector('#selectdir1').style.color = '#D70040'; ready = false;}
     if (!this.state.dir2Selected && this.state.operation == 1) {document.body.querySelector('#selectdir2').style.color = '#D70040'; ready = false;}
@@ -109,7 +126,92 @@ export default class App extends React.Component {
     } else {
       queryBr.style.color = '#000000';
     }
+
+    if (true) {
+      let taskCount = this.state.allTasks + 1;
+      let operations = this.state.operations; operations.push(this.state.operation)
+      let dirs1 = this.state.dirs1; dirs1.push(this.state.dir1);
+      let dirs2 = this.state.dirs2; dirs2.push(this.state.dir2);
+      let dirs3 = this.state.dirs3; dirs3.push(this.state.dir3);
+      let xCrop = this.state.x; xCrop.push(x);
+      let yCrop = this.state.y; yCrop.push(y);
+      let humanReadableOperation = '';
+      let allbr = this.state.br; allbr.push(br);
+      console.log(this.state.allTasks, taskCount, this.state.operations)
+      switch (this.state.operation) {
+        case 0: {humanReadableOperation = `Crop ${this.state.dir1} images to ${this.state.dir3} with dimensions {${x}, ${y}}`;break;}
+        case 1: {humanReadableOperation = `Merge ${this.state.dir1} images with ${this.state.dir2} to ${this.state.dir3}`;break;}
+        case 2: {humanReadableOperation = `Brightness ${this.state.dir1} images to ${this.state.dir3} by ${br}`;break;}
+      }
+      let HROperations = this.state.humanReadableOperations;
+      HROperations.push(humanReadableOperation);
+      this.setState({allTasks: taskCount, operations: operations, dirs1: dirs1, dirs2: dirs2, dirs3: dirs3, x: xCrop, y: yCrop, humanReadableOperations: HROperations, br: allbr});
+      console.log(this.state.humanReadableOperations)
+    }
     
+  }
+
+  ViewTasks = () => {
+    let taskSwitch = this.state.viewTasks;
+    this.setState({viewTasks: !taskSwitch});
+  }
+  
+  DeleteTask = (ind) => {
+    let newState = this.state;
+    newState.operations.splice(ind, 1);
+    newState.dirs1.splice(ind, 1);
+    newState.dirs2.splice(ind, 1);
+    newState.dirs3.splice(ind, 1);
+    newState.humanReadableOperations.splice(ind, 1);
+    newState.x.splice(ind, 1);
+    newState.y.splice(ind, 1);
+    newState.br.splice(ind, 1);
+    newState.allTasks--;
+    console.log(newState);
+    this.setState(newState);
+    console.log(this.state);
+  }
+
+  ProgressOneItem = async (ind) => {
+    document.body.querySelectorAll('button').forEach((element) =>{
+      element.style.backgroundColor = '#808080'
+      element.disabled = true;
+    })
+    switch (this.state.operations[ind]) {
+      case 0: {
+        await StartCropProcess(this.state.dirs1[ind], this.state.dirs3[ind], this.state.x[ind], this.state.y[ind]);
+        break;
+      }
+      case 1: {
+        await StartMergeProcess(this.state.dirs1[ind], this.state.dirs2[ind], this.state.dir3[ind]);  
+        break;
+      }
+      case 2: {
+        await StartProcessBrightness(this.state.dirs1[ind], this.state.dirs3[ind], this.state.br[ind]); 
+        break;
+      }
+    }
+    document.body.querySelectorAll('button').forEach((element) =>{
+      element.style.backgroundColor = '#2ea44f'
+      element.disabled = false;
+    })
+  };
+
+  Progress = async () => {
+    this.setState({ isRunning: 1 });
+    
+    for (let i = 0; i < this.state.operations.length; i++) {
+      let hr = this.state.humanReadableOperations[i];
+      this.setState({ currentOperationLabel: hr });
+  
+      await this.ProgressOneItem(i); // Notice the use of await here
+  
+      console.log("Called");
+      this.setState({ completedTasks: i+1 });
+    }
+    
+    this.setState({ isRunning: 0 });
+    /*
     if (ready) {
       document.body.querySelectorAll('button').forEach((element) =>{
         element.style.backgroundColor = '#808080'
@@ -127,14 +229,31 @@ export default class App extends React.Component {
         element.style.backgroundColor = '#2ea44f'
         element.disabled = false;
       })
-    }
+    }*/
   };
   
 
   render() {
     return (
       <div id='App'>
-        <div className='selectDataset'>
+        <div className='scheduler'>
+          <div className='sch_left'>
+          Tasks: {this.state.completedTasks}/{this.state.allTasks}&emsp;Task progress: 0%&emsp;<button className='viewTaskButton' onClick={this.ViewTasks}>{`${this.state.viewTasks ? 'Hide tasks' : 'View tasks'}`}</button><br></br> {this.state.currentOperationLabel}
+          </div>
+          <div className='sch_right'>
+            <button onClick={this.Progress}>Run all</button>
+          </div>
+        </div>
+        <div className='line'></div>
+        <div className={`${this.state.viewTasks ? 'tasksView' : 'hide'}`}>
+          <h1>Tasks</h1>
+          <ol>
+            {this.state.operations.map((hr, ind) => (
+              <li key={'li_'+ind}><div className='taskViewLeft'>{ind+1}. {this.state.humanReadableOperations[ind]}</div><div className='taskViewRight'><button className={`${this.state.completedTasks + this.state.isRunning <= ind ? 'taskViewRight' : 'hide'}`} key={'button_'+ind} onClick={() => this.DeleteTask(ind)}>Delete</button></div></li>
+            ))}
+          </ol>
+        </div>
+        <div className={`${this.state.viewTasks ? 'hide' : 'selectDataset'}`}>
           <div className='operation'><h2>Operation</h2></div>
           <p></p>
           <div>
@@ -235,7 +354,7 @@ export default class App extends React.Component {
             </div>
           </div>
           <div>
-            <button className='start' onClick={this.Progress}>Start</button>
+            <button className='start' onClick={this.AddToQueue}>Add to queue</button>
           </div>
         </div>
       </div>
